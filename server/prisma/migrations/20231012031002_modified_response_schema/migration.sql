283e903c7915c38c280d549a810a5f3705d9e755
dipanshu18 <torawanedipanshu@gmail.com>
2024-02-21 17:09:52 +0530

    Shifting project to Nextjs

--- server/prisma/migrations/20231012031002_modified_response_schema/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `validatingQuestion` to the `Response` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Response" ADD COLUMN     "validatingQuestion" TEXT NOT NULL;
